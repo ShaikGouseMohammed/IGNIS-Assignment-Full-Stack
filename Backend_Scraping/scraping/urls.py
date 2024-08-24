ec8a6575422005918eb6a9884ac3ac6cbf8f28a8
ShaikGouseMohammed <gouse123in@gmail.com>
2024-08-24 11:04:08 +0530

    Delete Backend_Scraping directory

--- Backend_Scraping/scraping/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import ProductListAPIView
-
-urlpatterns = [
-    path('api/products/', ProductListAPIView.as_view(), name='product-list'),
-]
--- Backend_Scraping/scraping/views.py
+++
@@ -1,8 +0,0 @@
-# views.py
-from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
-
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
